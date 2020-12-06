28a231fe05ef2b5425879db37f70b55570627268
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-12-06 21:46:33 +0400

    add dynamic data

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
+
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -10,23 +13,29 @@ export class MainViewComponent implements OnInit {
 
   constructor() { }
 
+  board: Board = new Board("ERP Board", [
+    new Column("Ideas", [
+      "Implement ERP"
+    ]),
+    new Column("Research", [
+      "Vendor Selection",
+    ]),
+    new Column("Todo", [
+      "RFP",
+      "Technical Requirements",
+      "Infrastructure Sizing"
+    ]),
+    new Column("Done", [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ]),
+  ]);
+
   ngOnInit(): void {
   }
-
-  todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
-
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
